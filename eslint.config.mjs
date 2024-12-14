af9c41ce55b8d44e67279cf3851f9e5d40fb6a7e
hellowurld-create <madebyaugus@gmail.com>
2024-12-14 02:11:39 +0100

    config: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,18 @@
 /** @format */
 
 import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	// import.meta.dirname is available after Node.js v20.11.0
-	baseDirectory: import.meta.dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-	...compat.config({
-		extends: ['next'],
-		settings: {
-			next: {
-				rootDir: 'packages/my-app/',
-			},
-		},
-	}),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
 ];
 
 export default eslintConfig;
